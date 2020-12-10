27eb75f545b8f8fae91e9fcf99fdea0a9bfa58a8
Illia Shakin <S00188372@mail.itsligo.ie>
2020-12-10 18:10:35 +0000

    Completed interface and working on maths

--- CA2/Employee.cs
+++ CA2/Employee.cs
@@ -12,16 +12,30 @@ namespace CA2
 
         public string LastName { get; set; }
 
-        public Employee(string firstname, string lastname)
+        public string EmployeeTime { get; set; }
+
+        public decimal FullTimeSalary { get; set; }
+
+        public decimal HourlyRate { get; set; }
+
+        public double Hours { get; set; }
+
+
+
+        public Employee(string firstname, string lastname, string employeetime, decimal fulltimesalary, decimal hourlyrate, double hours)
         {
             FirstName = firstname;
             LastName = lastname;
+            EmployeeTime = employeetime;
+            FullTimeSalary = fulltimesalary;
+            HourlyRate = hourlyrate;
+            Hours = hours;
 
         }
 
         public override string ToString()
         {
-            return string.Format($"{LastName}, {FirstName}");
+            return string.Format($"{LastName}, {FirstName},{EmployeeTime}");
         }
     }
 }
--- CA2/MainWindow.xaml.cs
+++ CA2/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CA2
 {
@@ -22,7 +11,11 @@ namespace CA2
     public partial class MainWindow : Window
     {
 
+        public string EmployeeTime = "";
+        public static decimal FtSalary;
+
         ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+        ObservableCollection<Employee> FilteredEmployees = new ObservableCollection<Employee>();
         public MainWindow()
         {
             InitializeComponent();
@@ -30,13 +23,17 @@ namespace CA2
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Employee emp1 = new Employee("Jane", "Jones");
-            Employee emp2 = new Employee("Joe", "Murphy");
+            Employee emp1 = new Employee("Jane", "Jones", "Full Time",100000, 5,12.5);
+            Employee emp2 = new Employee("Bob", "Reilly", "Full Time", 150000,5,12.5);
+            Employee emp3 = new Employee("Joe", "Murphy", "Part Time", 9000,5,12.5);
 
             employees.Add(emp1);
             employees.Add(emp2);
+            employees.Add(emp3);
 
             lbxEmployees.ItemsSource = employees;
+
+
 
         }
 
@@ -56,16 +53,135 @@ namespace CA2
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
 
+
             //Reading From inputs
-            string firstName = tbxFirstName.Text;
+            string FirstName = tbxFirstName.Text;
             string LastName = tbxLastName.Text;
+            decimal Hours = Convert.ToInt32(tbxHoursWorked.Text);
+            double Rate = Convert.ToInt32(tbxHourlyRate.Text);
+            FtSalary = Convert.ToInt32(tbxSalary.Text);
+
+
+            string FullTime = "Full Time";
+            if (rbtFT.IsChecked == true)
+            {
+                EmployeeTime = FullTime;
+            }
+            else
+            {
+                EmployeeTime = "Part Time";
+            }
+
 
             //Create Object
-            Employee employee = new Employee(firstName, LastName);
+            Employee employee = new Employee(FirstName, LastName, EmployeeTime,FtSalary, Hours,Rate );
 
 
             //Add to observable collection
             employees.Add(employee);
+
+
+        }
+
+        private void lbxEmployees_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Checking what has been selected
+            Employee selectedEmployee = lbxEmployees.SelectedItem as Employee;
+
+            //Check it is not null
+            if (selectedEmployee != null)
+            {
+                tbxFirstName.Text = selectedEmployee.FirstName;
+                tbxLastName.Text = selectedEmployee.LastName;
+
+                if(selectedEmployee.EmployeeTime == "Full Time")
+                {
+                    rbtFT.IsChecked = true;
+                    decimal pay = (selectedEmployee.FullTimeSalary / 12);
+                    tblkPay.Text = pay.ToString();
+                    tbxSalary.Text = selectedEmployee.FullTimeSalary.ToString();
+                }
+                else
+                {
+                    tbxSalary.Text = "";
+                    rbtPT.IsChecked = true;
+                }
+
+            }
+
+        }
+
+        private void rbtFT_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cbxFullTime_Checked(object sender, RoutedEventArgs e)
+        {
+
+            if ((cbxPartTime.IsChecked == true) && (cbxFullTime.IsChecked == true)) //Checking if both are checked
+            {
+                foreach (Employee employee in employees) //looping through emloyees
+                {
+                    if (employee.EmployeeTime == "Full Time")
+                    {
+                        FilteredEmployees.Add(employee);
+                    }
+                    else if (employee.EmployeeTime == "Part Time")
+                    {
+                        FilteredEmployees.Add(employee);
+                    }
+                    FilteredEmployees.Clear();
+                }
+            }
+            //If sequence to check for the employee job time
+            else if (cbxFullTime.IsChecked == true) // Check if Full time is ticked
+            {
+                EmployeeTime = "Full Time";
+                foreach (Employee employee in employees) //looping through emloyees
+                {
+                    if (employee.EmployeeTime == "Full Time")
+                    {
+                        FilteredEmployees.Add(employee);
+                    }
+                }
+            }
+            else if (cbxPartTime.IsChecked == true) //check if part time is ticked
+            {
+                EmployeeTime = "Part Time";
+                foreach (Employee employee in employees) //looping through emloyees
+                {
+                    if (employee.EmployeeTime == "Part Time")
+                    {
+                        FilteredEmployees.Add(employee);
+                    }
+                }
+            }
+            else
+            {
+                lbxEmployees.ItemsSource = employees;
+            }
+
+
+            lbxEmployees.ItemsSource = FilteredEmployees;
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            tbxFirstName.Clear();
+            tbxLastName.Clear();
+            rbtFT.IsChecked = false;
+            rbtPT.IsChecked = false;
+            tbxHourlyRate.Clear();
+            tbxHoursWorked.Clear();
+            tbxSalary.Clear();
+        }
+
+        private void tblkPay_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+
 
         }
     }
